93ea0d2c4794a924ceac0b7655af361b2117ce62
Rick Bradley <rick@rickbradley.com>
2015-02-08 13:48:25 -0600

    extract to classes
    the beginnings of a refactoring

---
+++ lib/fitblitzed/config.rb
@@ -0,0 +1,48 @@
+require "rubygems"
+require "bundler/setup"
+require "yaml"
+
+module FitBlitzed
+  class ConfigService
+    attr_reader :config, :handle
+
+    def initialize(config, handle)
+      raise ArgumentError, "configuration does not include data for service [#{handle}]" unless config.config[handle]
+      @config = config
+      @handle = handle
+    end
+
+    def method_missing(meth, *args)
+      # predicate methods: is the key in the configuration?
+      if meth[-1] == "?"
+        config.config[handle].has_key?(meth[0..-2].to_sym)
+      else
+        # return the value if the key is in the configuration
+        if config.config[handle].has_key?(meth)
+          config.config[handle][meth]
+        end
+      end
+    end
+  end
+
+  class Config
+    attr_reader :config
+
+    def service(handle)
+      FitBlitzed::ConfigService.new(self, handle)
+    end
+
+    def config
+      @config ||= read
+    end
+
+    def read
+      @config = begin
+        YAML.load(File.open("config.yml"))
+      rescue ArgumentError => e
+        puts "Could not parse YAML: #{e.message}"
+        exit
+      end
+    end
+  end
+end
---
+++ lib/fitblitzed/insight_timer.rb
@@ -0,0 +1,79 @@
+require "rubygems"
+require "bundler/setup"
+require "mechanize"
+require "csv"
+require "fitblitzed/config"
+
+module FitBlitzed
+  class InsightTimer
+    attr_reader :config
+
+    def initialize
+      config = FitBlitzed::Config.new
+      raise ArgumentError, "Configuration required" unless config
+      raise ArgumentError, ":insight_timer configuration required" unless @config = config.service(:insight_timer)
+      raise ArgumentError, ":email configuration value is required" unless @config.email?
+      raise ArgumentError, ":password configuration value is required" unless @config.password?
+    end
+
+    def email
+      config.email
+    end
+
+    def password
+      config.password
+    end
+
+    def minutes_meditated(target_date)
+      data = fetch_meditation_csv
+
+      tally = 0
+      while day = data.shift
+        next if day.empty?
+
+        date_time, minutes, description = *day
+        next unless date_time =~ %r{^\d+/\d+/\d+\s+\d+:\d+:\d+$}
+
+        if is_date?(date_time, target_date)
+          tally += minutes.to_i
+        else
+          break
+        end
+      end
+      tally
+    end
+
+    def fetch_meditation_csv
+      csv = nil
+
+      if ENV['DEVELOPMENT']
+        # FIXME -- I'm on a :plane:!
+        csv = File.read("/Users/rick/Downloads/insight_connect_export.csv")
+      else
+        agent = Mechanize.new do |a|
+          a.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36"
+        end
+
+        agent.get("https://insighttimer.com/") do |page|
+          # Submit the login form
+          my_page = page.form_with(:name => "login_form") do |form|
+            form.field_with(:name => "user_session[email]").value = email
+            form.field_with(:name => "user_session[password]").value = password
+          end.click_button
+
+          download = "https://insighttimer.com/users/export"
+
+          agent.pluggable_parser.default = Mechanize::Download
+          csv = agent.get(download).body
+        end
+      end
+
+      Array(CSV.parse(csv))
+    end
+
+    def is_date?(date_time_string, target_date)
+      # TODO: make this more robust against time zones, post-midnight times
+      Date.strptime(date_time_string, "%m/%d/%Y") == target_date
+    end
+  end
+end
